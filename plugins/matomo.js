4a0f7ccec004707c9e37021b5ca9699ccca1cedf
PanierAvide <panieravide@riseup.net>
2021-03-23 10:26:28 +0100

    Improve page tracking

--- plugins/matomo.js
+++ plugins/matomo.js
@@ -1,15 +1,17 @@
+import { decode } from '../lib/url';
+
 export default ({ app }) => {
-  let firstLoad = true;
   app.router.afterEach((to, from) => {
-    if (to.name === 'index' && !firstLoad) {
+    if (to.name === 'index' && decode(from.path).filter === decode(to.path).filter) {
       return;
     }
     if (to.name === 'place' && to.name === from.name && to.params.id === from.params.id) {
       return;
     }
-    firstLoad = false;
+
     const _paq = window._paq || [];
     window._paq = _paq;
+    window._paq.push(['setCustomUrl', to.path]);
     window._paq.push(['trackPageView']);
   });
 };
